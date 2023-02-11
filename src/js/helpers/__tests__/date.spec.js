fd8603017e0e6d3a8b9294e04a13b9b610be9de5
ElijahBond <bondarefil@gmail.com>
2023-02-11 18:08:38 +0300

    add location test

--- src/js/helpers/__tests__/date.spec.js
+++ src/js/helpers/__tests__/date.spec.js
@@ -1,7 +1,7 @@
-import { formatDate } from './date';
+import { formatDate } from '../date';
 
 describe('formatDate', () => {
-  it('check format', () => {
+  test('check format', () => {
     expect(formatDate(1577014368252, 'yyyy')).toBe('2019');
   });
 });
